e795de3442085d473c6a8525d61afe252af8abc5
Rezzan10 <you@example.com>
2023-11-23 22:23:40 +0100

    add api get requst, implement chart js

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///temperature_data.db'
 db = SQLAlchemy(app)
 
@@ -16,6 +18,23 @@ class TemperatureData(db.Model):
 @app.route("/")
 def client():
     return ""
+
+@app.route('/api/temperatures', methods=['GET'])
+def get_all_temperature():
+    data = TemperatureData.query.all()
+
+    temperature_list = []
+    for entry in data:
+        temperature_list.append({
+            'id': entry.id,
+            'date': entry.date.strftime('%Y-%m-%d'),
+            'time': entry.time.strftime('%H:%M:%S'),
+            'temperature': entry.temperature,
+            'humidity': entry.humidity
+        })
+
+    return jsonify({'data': temperature_list})
+
 
 @app.route('/api/temperature', methods=['POST'])
 def add_temperature():
@@ -38,9 +57,13 @@ def add_temperature():
     db.session.commit()
 
     return jsonify({'message': 'Temperature data added successfully'}), 201
+    
 
 if __name__ == "__main__":
     with app.app_context():
         print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
         db.create_all()
         app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,11 +1,49 @@
-<script lang="ts">
+<script>
+  import Chart from 'chart.js/auto'
+  import { onMount } from 'svelte';
 
+  /**
+     * @type {any[]}
+     */
+  let chartValues = [];
+  /**
+     * @type {any[]}
+     */
+  let chartLabels = [];
+  let ctx;
+  /**
+     * @type {HTMLCanvasElement}
+     */
+  let chartCanvas;
+
+  async function fetchData() {
+    const response = await fetch('http://127.0.0.1:5000/api/temperatures');
+    const data = await response.json();
+
+    chartLabels = data.data.map((/** @type {{ date: any; }} */ entry) => entry.date);
+    chartValues = data.data.map((/** @type {{ temperature: any; }} */ entry) => entry.temperature);
+  }
+
+  onMount(async () => {
+    await fetchData();
+    ctx = chartCanvas.getContext('2d');
+
+    // @ts-ignore
+    var chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: chartLabels,
+        datasets: [
+          {
+            label: 'Temperatur',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: chartValues
+          }
+        ]
+      }
+    });
+  });
 </script>
 
-<main>
-  <h1>Test</h1>
-
-</main>
-
-<style>
-</style>
+<canvas bind:this={chartCanvas} id="myChart"></canvas>
