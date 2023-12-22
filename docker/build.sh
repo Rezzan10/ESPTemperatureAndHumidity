8b979960bbdefb659e016dade0c3be25c2abc417
Rezzan10 <rezzano@protonmail.com>
2023-12-22 12:19:04 +0100

    add ssl

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
-docker build -t pbrunke/temperature-api:1.0.0 -f ./docker/api/Dockerfile ./api
-docker build -t pbrunke/temperature-server:1.0.0 -f ./docker/nginx/Dockerfile .
+docker build -t rezzan/temperature-api:1.0.0 -f ./docker/api/Dockerfile ./api
+docker build -t rezzan/temperature-server:1.0.0 -f ./docker/nginx/Dockerfile .
